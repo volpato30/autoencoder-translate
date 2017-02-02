1c705c7e79d49a77ae0e7f52ed93c76b024d21a5
rqiao <volpato30@gmail.com>
2017-02-02 16:37:02 -0500

    fix;

--- generate_feature.py
+++ generate_feature.py
@@ -32,12 +32,23 @@ tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
 tf.app.flags.DEFINE_integer("batch_size", 64,
                                                         "Batch size to use during training.")
 tf.app.flags.DEFINE_integer("size", 256, "Size of each model layer.")
-tf.app.flags.DEFINE_integer("language_id", 0, "0 for English, 1 for French")
 tf.app.flags.DEFINE_integer("num_layers", 1, "Number of layers in the model.")
 tf.app.flags.DEFINE_integer("en_vocab_size", 40000, "English vocabulary size.")
 tf.app.flags.DEFINE_integer("fr_vocab_size", 40000, "French vocabulary size.")
 tf.app.flags.DEFINE_string("data_dir", "/tmp", "Data directory")
 tf.app.flags.DEFINE_string("train_dir", "/tmp", "Training directory.")
+tf.app.flags.DEFINE_integer("max_train_data_size", 0,
+                                                        "Limit on the size of training data (0: no limit).")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 200,
+                                                        "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_boolean("decode", False,
+                                                        "Set to True for interactive decoding.")
+tf.app.flags.DEFINE_boolean("self_test", False,
+                                                        "Run a self-test if this is set to True.")
+tf.app.flags.DEFINE_boolean("use_fp16", False,
+                                                        "Train using fp16 instead of fp32.")
+
+
 
 
 FLAGS = tf.app.flags.FLAGS
