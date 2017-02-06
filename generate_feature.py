30809c5a7a16902fb3e0cf09129d3c0b95e16c2f
rqiao <volpato30@gmail.com>
2017-02-05 23:15:13 -0500

    fix

--- generate_feature.py
+++ generate_feature.py
@@ -24,32 +24,6 @@ import seq2seq_model
 from translate_en2en import create_model
 import scipy.io as sio
 
-
-tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
-tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99,
-                                                    "Learning rate decays by this much.")
-tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
-                                                    "Clip gradients to this norm.")
-tf.app.flags.DEFINE_integer("batch_size", 64,
-                                                        "Batch size to use during training.")
-tf.app.flags.DEFINE_integer("size", 256, "Size of each model layer.")
-tf.app.flags.DEFINE_integer("num_layers", 1, "Number of layers in the model.")
-tf.app.flags.DEFINE_integer("en_vocab_size", 40000, "English vocabulary size.")
-tf.app.flags.DEFINE_integer("fr_vocab_size", 40000, "French vocabulary size.")
-tf.app.flags.DEFINE_string("data_dir", "/tmp", "Data directory")
-tf.app.flags.DEFINE_string("train_dir", "/tmp", "Training directory.")
-tf.app.flags.DEFINE_integer("max_train_data_size", 0,
-                                                        "Limit on the size of training data (0: no limit).")
-tf.app.flags.DEFINE_integer("steps_per_checkpoint", 200,
-                                                        "How many training steps to do per checkpoint.")
-tf.app.flags.DEFINE_boolean("decode", False,
-                                                        "Set to True for interactive decoding.")
-tf.app.flags.DEFINE_boolean("self_test", False,
-                                                        "Run a self-test if this is set to True.")
-tf.app.flags.DEFINE_boolean("use_fp16", False,
-                                                        "Train using fp16 instead of fp32.")
-tf.app.flags.DEFINE_boolean("basic_rnn", True, "using basic rnn or attention model")
-
 tf.app.flags.DEFINE_integer("language_id", 0, "0 for english, 1 for french")
 
 
